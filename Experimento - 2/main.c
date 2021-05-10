bb5435f1e8a3f2d52ca213a27927e4469fdaac7c
Noel Prado <68123083+Pra19025@users.noreply.github.com>
2021-05-10 09:41:33 -0600

    lab terminado

--- Experimento - 2/main.c
+++ Experimento - 2/main.c
@@ -11,12 +11,17 @@
 #include "driverlib/interrupt.h"
 #include "driverlib/gpio.h"
 #include "driverlib/timer.h"
+#include "driverlib/uart.h"
 
 uint32_t loadval;
-uint8_t control = 0;
-uint8_t control2 = 0;
+
+uint8_t entrada;
+uint8_t rojo = 0;
+uint8_t verde = 0;
+uint8_t azul = 0;
 
 void toggle(void);
+void entradaUART(void);
 
 int main(void)
 {
@@ -48,53 +53,76 @@ int main(void)
 
     IntRegister(INT_TIMER0A, toggle);
 
-    GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1, 2);
-
     //configuración UART
-
-
 
     SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
 
-
-    GPIOPinConfigure(GPIO_PA0_U0RX);
-    GPIOPinConfigure(GPIO_PA1_U0TX);
     GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
 
     UARTConfigSetExpClk(UART0_BASE, SysCtlClockGet(), 115200,
     UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE |
     UART_CONFIG_PAR_NONE);
 
+    UARTFIFOLevelSet(UART0_BASE, UART_FIFO_TX1_8, UART_FIFO_RX1_8);
 
+    UARTEnable(UART0_BASE);
+    IntEnable(INT_UART0);
+    UARTIntEnable(UART0_BASE, UART_INT_RX);
+
+// se hace que se vaya a la función especificada en la interrupción del uart
+    IntRegister(INT_UART0, entradaUART);
+
+    IntMasterEnable();
 
     while (1)
     {
+        if (entrada == 'r')
+        {
+            rojo = ~rojo & GPIO_PIN_1;
+            entrada = 0;
+        }
 
+        if (entrada == 'b')
+        {
+            azul = ~azul & GPIO_PIN_2;
+            entrada = 0;
+        }
+        if (entrada == 'g')
+        {
+            verde = ~verde & GPIO_PIN_3;
+            entrada = 0;
+        }
 
     }
 
     return 0;
 }
 
-
 //esta rutina hace el toggle a 0.5hz
 void toggle(void)
 {
 
 
-    if (control == 0)
-    {
         TimerIntClear(TIMER0_BASE, TIMER_TIMA_TIMEOUT);
-        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1,
-                     ~GPIOPinRead(GPIO_PORTF_BASE, GPIO_PIN_1));
+        //GPIOPinWrite tiene como parametros, puerto, pines y valor a escribir
+        GPIOPinWrite(
+                GPIO_PORTF_BASE,
+                rojo | verde | azul,
+                ~GPIOPinRead(GPIO_PORTF_BASE,
+                             GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3));
 
 
-    }
 
+}
 
+//rutina para la entrada del UART
 
+void entradaUART(void)
+{
 
+    UARTIntClear(UART0_BASE, UART_INT_RX);
+    entrada = UARTCharGetNonBlocking(UART0_BASE);
 
 }
 
