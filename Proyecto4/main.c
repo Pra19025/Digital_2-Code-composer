dc47afe97e384bc3b52e6010792a44b7a9a1dc9c
Noel Prado <68123083+Pra19025@users.noreply.github.com>
2021-05-11 19:33:24 -0600

    no lee los botones

--- Proyecto4/main.c
+++ Proyecto4/main.c
@@ -1,9 +1,68 @@
+//Noel Prado
+//19025
+//Proyecto 4
 
+#include <stdint.h>
+#include <stdbool.h>
+#include "inc/tm4c123gh6pm.h"
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+#include "inc/hw_ints.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/gpio.h"
+#include "driverlib/timer.h"
+#include "driverlib/uart.h"
 
-/**
- * main.c
- */
 int main(void)
 {
-	return 0;
+    //configuración del reloj
+    // a 40Mhz
+    SysCtlClockSet(
+    SYSCTL_SYSDIV_5 | SYSCTL_USE_PLL | SYSCTL_XTAL_16MHZ | SYSCTL_OSC_MAIN);
+
+    //se configuran los pines como salidas (para los leds)
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+    GPIOPinTypeGPIOOutput(
+            GPIO_PORTA_BASE,
+            GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3 | GPIO_PIN_4
+                    | GPIO_PIN_5 | GPIO_PIN_6 | GPIO_PIN_7);
+
+    //se configuran los pines como entradas (para los push buttons)
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOE);
+    GPIOPinTypeGPIOInput(GPIO_PORTE_BASE,
+                         255);
+
+    //se configuran las pull up
+    GPIOPadConfigSet(GPIO_PORTE_BASE,
+                     255,
+                     GPIO_STRENGTH_8MA,
+                     GPIO_PIN_TYPE_STD_WPU);
+
+    //GPIOPinWrite(GPIO_PORTA_BASE, 0b1100, 0b1100);
+
+
+    int32_t boton1 = 16;
+    int32_t boton2 = 16;
+
+
+    //Loop forever
+    while(1){
+
+        SysCtlDelay(1000);
+        //escritura de prueba
+        GPIOPinWrite(GPIO_PORTA_BASE, 0b0100, 0b1111);
+        boton1 = boton2;
+        boton2 = GPIOPinRead(GPIO_PORTE_BASE, 0b1111);
+
+        if(boton1 == 0 && boton2 == 16){
+            //escritura de prueba
+                  GPIOPinWrite(GPIO_PORTA_BASE, 0b0000, 0b1111);
+            GPIOPinWrite(GPIO_PORTA_BASE, 0b1111, 0b1111);
+        }
+
+
+    }
+
+    return 0;
 }
